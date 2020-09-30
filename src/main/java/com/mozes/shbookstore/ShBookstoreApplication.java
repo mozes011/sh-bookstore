7062f5b54776f03447e1417ddae378a50341f224
Yair <yar@feelter.com>
2020-09-30 18:57:35 +0300

    initial commit

---
+++ src/main/java/com/mozes/shbookstore/ShBookstoreApplication.java
@@ -0,0 +1,13 @@
+package com.mozes.shbookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShBookstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShBookstoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mozes/shbookstore/ShBookstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mozes.shbookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShBookstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
